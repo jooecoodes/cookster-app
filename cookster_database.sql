ccfa216caeaee18a84de72425e8cac5a6a5ab71a
jooecoodes <jefeljoevillacorta@gmail.com>
2023-12-28 02:35:22 -0800

    Dec. 27, modified db

--- cookster_database.sql
+++ cookster_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2023 at 05:30 AM
+-- Generation Time: Dec 28, 2023 at 11:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,8 +18,51 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mathwiz_database`
+-- Database: `cookster_database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `video` text NOT NULL,
+  `content` text NOT NULL,
+  `difficulty` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `video`, `content`, `difficulty`) VALUES
+(5, 'Abra ', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/svebHG7qEXc?si=SCrOpBJAtMY_Pg6X\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>', 'asdasdasdasasd123123123', 'easy'),
+(7, 'Chocolate factory - KUNG IKAY AKIN', '<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Akdr8klkgl4?si=a7ne6nx-hGEm9r7G\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>', 'sdasdasdasdasdasdasdasd', 'easy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `id` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `choices` varchar(255) NOT NULL,
+  `answer` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`id`, `question`, `choices`, `answer`) VALUES
+(9, 'asdasd', 'asdasdasd', 'asdasd'),
+(10, 'asadas', 'dasdasdasd', 'asdasdasdasd');
 
 -- --------------------------------------------------------
 
@@ -53,13 +96,13 @@ INSERT INTO `user` (`token`, `id`, `badges`, `userprofile`, `useremail`, `gender
 ('', 1, '', NULL, 'dmsad@gmail.com', NULL, '', '', 'okay\r\n', 'ssample', 12, '', NULL, 1, 'user'),
 ('9409b587374c6fc6309487cef95c645c', 2, '', NULL, 'try@gmail.com', NULL, '', '', 'okayokay', 'charlie123123', 7, NULL, NULL, NULL, 'user'),
 ('', 3, '', NULL, 'okkaayo@gmail.com', NULL, '', '', 'omaewamou', 'asidasdadsadasd', 4, NULL, NULL, NULL, 'user'),
-('1de65c9c539d8311c9ce5f19b7fa5d91', 4, 'beta-tester-badge,verified-badge', 'user=4.jpg', 'clarito@gmail.com', NULL, 'nick', 'clarito', 'clarito123', 'clarito@123', 13, NULL, NULL, NULL, 'admin'),
+('a355bacc1243940985adc7e429957a38', 4, 'beta-tester-badge,verified-badge', 'user=4.jpg', 'clarito@gmail.com', NULL, 'nick', 'clarito', 'clarito123', 'clarito@123', 13, NULL, NULL, NULL, 'admin'),
 ('', 15, '', NULL, 'asd2@gmail.com', NULL, '', '', '', 'jefeljoe@', 0, NULL, '2023-11-20', 1, 'user'),
 ('', 16, '', NULL, 'asd@yahoo.com', NULL, 'ff', 'as', '', 'asd@yahoo.com', 0, NULL, '2023-11-20', 1, 'user'),
 ('', 17, '', NULL, 'jefeljoevillacorta@gmail.com', NULL, 'Albert', 'Swift', '', 'hoho@12345', 0, NULL, '2023-11-24', 1, 'user'),
 ('93761e3d0e5083ca850428a39aa970d8', 18, '', NULL, 'lol@gmail.com', 'female', 'Taylor', 'Swift', '', 'lol@gmail.com', 0, NULL, '2023-11-24', 1, 'user'),
 ('', 19, '', NULL, 'janedoe@gmail.com', NULL, 'Jane', 'Doe', '', 'janedoe123@', 0, NULL, '2023-11-24', 1, 'user'),
-('2aeb1eae0fc751c4dd42ad50de78a0d2', 20, '', 'user=20.jpg', 'jameswayne@gmail.com', '', 'James', 'Wayne', '', 'jameswayne@gmail.com', 0, NULL, '2023-11-24', 1, 'user'),
+('78d070de00f37d04a5ee035289237fc8', 20, '', 'user=20.jpeg', 'jameswayne@gmail.com', '', 'Mike', 'Swift', '', 'jameswayne@gmail.com', 0, NULL, '2023-11-24', 1, 'user'),
 ('', 21, '', NULL, 'jameswhite@gmail.com', '', 'James', 'White', '', 'arnoldswift@', 0, NULL, '2023-11-29', 1, 'user'),
 ('', 22, '', NULL, 'johnnybravo@gmail.com', 'lol', 'johnny', 'bravo', '', 'johnnybravo@', 0, NULL, '2023-11-29', 1, 'user'),
 ('', 23, '', NULL, 'johnnyuts@gmail.com', '', 'johnny', 'uts', '', 'johnnyuts@', 0, NULL, '2023-11-29', 1, 'user'),
@@ -69,11 +112,26 @@ INSERT INTO `user` (`token`, `id`, `badges`, `userprofile`, `useremail`, `gender
 ('', 27, '', NULL, 'okok@gmail.com', 'lol', 'ok', 'ok', '', 'asdasd@123', 0, NULL, '2023-11-29', 1, 'user'),
 ('', 28, '', NULL, 'soso@gmail.com', 'lol', 'soso', 'soso', '', 'soso@123', 0, NULL, '2023-11-29', 1, 'user'),
 ('', 29, '', NULL, 'asdasdasdasd@gmail.com', 'lol', 'asdasd', 'asdasd', '', 'asdasdasdasd@', 0, NULL, '2023-11-29', 1, 'user'),
-('', 30, '', NULL, 'helloworld@gmail.com', 'male', 'hello', 'world', '', 'helloworld@', 0, NULL, '2023-11-29', 1, 'user');
+('', 30, '', NULL, 'helloworld@gmail.com', 'male', 'hello', 'world', '', 'helloworld@', 0, NULL, '2023-11-29', 1, 'user'),
+('e92f7db0b837a91ee5c2ecefb2036d22', 31, '', NULL, 'jefjoe@gmail.com', 'lol', 'Jefel', 'Joe', '', 'Jefeljoe@villacorta', 0, NULL, '2023-12-28', 1, 'user'),
+('', 32, '', NULL, 'helloworld2@gmail.com', 'male', 'Hello2', 'World2', '', 'hello@world', 0, NULL, '2023-12-28', 1, 'user'),
+('922a9ab5168cbe2135a425d841f4901d', 33, '', NULL, 'mikeytyson@gmail.com', NULL, 'mikey', 'tyson', '', 'mikey@tyson', 0, NULL, '2023-12-28', 1, 'user');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -86,10 +144,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
